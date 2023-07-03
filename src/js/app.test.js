b95ed40081c5756124894393ee2ad0b6ce2c2098
exceptr <98112445+exceptr@users.noreply.github.com>
2023-07-03 18:57:45 +0300

    init

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -1,9 +1,4 @@
-describe("Пример теста", () => {
-  test.each([
-    { str: "Hello!", expected: "Demo: Hello!" },
-    { str: "", expected: "Demo: " },
-    { str: 100, expected: "Demo: 100" },
-  ])("demo($str)", ({ str, expected }) => {
-    expect(demo(str)).toBe(expected);
-  });
+test("Value", () => {
+  const expectedResult = 2;
+  expect(expectedResult).toBe(2);
 });
